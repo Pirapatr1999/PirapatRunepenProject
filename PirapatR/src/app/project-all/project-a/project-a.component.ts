99b35843b0142214725c7ef8f8eebd83ffca3ee1
pirapatr@mindbase.co.th <108516431+Pirapatr1999@users.noreply.github.com>
2022-09-21 15:05:35 +0700

    PirpatR
    Add Project

---
+++ PirapatR/src/app/project-all/project-a/project-a.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-a',
+  templateUrl: './project-a.component.html',
+  styleUrls: ['./project-a.component.scss']
+})
+export class ProjectAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
